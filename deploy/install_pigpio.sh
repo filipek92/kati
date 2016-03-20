811b2f708145287267380ad8d8c0f3e595cbd90b
Tomas Bedrich <ja@tbedrich.cz>
2016-03-20 23:52:13 +0100

    Added pigpio installation

---
+++ deploy/install_pigpio.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# must run as root!
+
+set -x
+
+rm master.zip
+rm -rf pigpio-master
+wget https://github.com/joan2937/pigpio/archive/master.zip
+unzip master.zip
+cd pigpio-master
+make -j4
+make install
