72da104bcc99fa0b807523a3ad28e404916929e7
Tomas Bedrich <ja@tbedrich.cz>
2016-10-05 01:06:28 +0200

    Added SIGTERM and SIGINT processing

--- start.py
+++ start.py
@@ -3,20 +3,32 @@
 import kati.controller
 import time
 import logging
+import signal
+
+stop = False
+
+
+def shutdown():
+    global stop
+    stop = True
+
+# register shutdown handler
+signal.signal(signal.SIGINT, shutdown)
+signal.signal(signal.SIGTERM, shutdown)
+
 
 # start logger
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
-# init
+# init controller
 controller = kati.controller.Controller()
 log.info("started")
 
+
 try:
-    # main loop
-    while True:
-        time.sleep(300)
-except (KeyboardInterrupt, SystemExit):
-    # cleanup
+    while not stop:
+        time.sleep(1)
+finally:
     controller.shutdown()
     log.info("shutdown complete")
